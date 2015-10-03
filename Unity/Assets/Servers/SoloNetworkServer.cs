7a26b73849de7259a2044d28abbb6b03582e304b
FoKycHuK <fokychuk47@ya.ru>
2015-10-03 17:49:24 +0500

    abstract TCPserver

---
+++ Unity/Assets/Servers/SoloNetworkServer.cs
@@ -0,0 +1,20 @@
+﻿using CVARC.V2;
+
+
+namespace Assets
+{
+    class SoloNetworkServer : UnityServer
+    {
+        public SoloNetworkServer(int port) : base(port) { }
+
+        protected override void HandleClient(CvarcClient client)
+        {
+            Dispatcher.AddRunner(new NetworkRunner(client));
+        }
+
+        protected override void Print(string str)
+        {
+            Debugger.Log(DebuggerMessageType.Unity, "network server: " + str);
+        }
+    }
+}
---
+++ Unity/Assets/Servers/TournamentServer.cs
@@ -0,0 +1,20 @@
+﻿using CVARC.V2;
+
+
+namespace Assets
+{
+    class TournamentServer : UnityServer
+    {
+        public TournamentServer(int port) : base(port) { }
+
+        protected override void HandleClient(CvarcClient client)
+        {
+            TournamentPool.AddPlayerToPool(client);
+        }
+
+        protected override void Print(string str)
+        {
+            Debugger.Log(DebuggerMessageType.Unity, "tournament server: " + str);
+        }
+    }
+}
