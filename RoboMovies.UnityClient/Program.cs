13281e67854bee7df2a16bbac954fc91f637fd7b
fokychuk_book <fokychuk47@ya.ru>
2015-10-06 13:25:41 +0600

    activate popcorn do not working

--- RoboMovies.UnityClient/Program.cs
+++ RoboMovies.UnityClient/Program.cs
@@ -9,29 +9,46 @@
             // назначаем обработчик сенсоров
             client.SensorDataReceived += HandleSensorData;
             // указываем настройки
-            client.Configurate(14000, true, RoboMoviesBots.Stand, ip:"127.0.0.1");
+            client.Configurate(14000, true, RoboMoviesBots.Stand, ip: "127.0.0.1");
             Control(client);
         }
 
         static void Control(RMClient<FullMapSensorData> client)
         {
             // управляем роботом
-            client.Move(65);
-            client.Rotate(-90);
-            client.Stand(1);
-            client.GripPopCorn();
-            client.Rotate(-90);
-            client.Move(70);
-            client.Stand(1);
-            client.ReleasePopCorn();
+            //client.Move(65);
+            //client.Rotate(-90);
+            //client.Stand(1);
+            //client.GripPopCorn();
+            //client.Rotate(-90);
+            //client.Move(8);
 
+            //client.Rotate(-90);
+            //client.Stand(0.5);
+            //client.Move(-85);
+            //client.Stand(1);
+            //client.ActivatePopCornDispenser();
+            //client.Move(70);
+            //client.Rotate(90);
+            //client.Move(10);
+            //client.ReleasePopCorn();
+            client.Move(64);
+            client.Rotate(-90);
+            client.Stand(0.1);
+            client.GripPopCorn();
+            client.Rotate(90);
+            client.Move(-30);
+            client.Rotate(-90);
+            client.Move(-70);
+            client.Stand(0.1);
+            client.ActivatePopCornDispenser();
             //корректно завершаем работу
             client.Exit();
         }
 
         static void HandleSensorData(FullMapSensorData sensorData)
         {
-            
+
         }
     }
 }
