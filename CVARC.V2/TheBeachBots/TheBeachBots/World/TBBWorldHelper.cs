62335efbfeec53b799cac3b7b3b95870742b8fd4
Ivan Poroshin <niemandkun@yandex.ru>
2015-10-20 21:59:48 +0500

    Make methods work with Frame3D in compatibility purposes

--- CVARC.V2/TheBeachBots/TheBeachBots/World/TBBWorldHelper.cs
+++ CVARC.V2/TheBeachBots/TheBeachBots/World/TBBWorldHelper.cs
@@ -8,31 +8,31 @@ namespace TheBeachBots
 {
     public class TBBWorldHelper
     {
-        private readonly Frame2D buildingAreaCenter = new Frame2D(0, 25, Angle.Zero);
+        private readonly Frame3D buildingAreaCenter = new Frame3D(0, 25, 0);
 
-        public bool IsInsideStartingArea(Frame2D location, SideColor color)
+        public bool IsInsideStartingArea(Frame3D location, SideColor color)
         {
             return Math.Abs(location.X) > 150 - 30 && location.X * Sign(color) >= 0
                 && location.Y < 40 && location.Y > -10;
         }
 
-        public bool IsInsideBuildingArea(Frame2D location, SideColor color)
+        public bool IsInsideBuildingArea(Frame3D location, SideColor color)
         {
             return Distance(location, buildingAreaCenter) < 60
                 && location.Y < 25 && location.X * Sign(color) >= 0;
         }
 
-        public bool IsInsideWater(Frame2D location)
+        public bool IsInsideWater(Frame3D location)
         {
             return location.X < -100 && Math.Abs(location.X) > 57.2 && Math.Abs(location.X) < 100.2;
         }
 
-        public bool IsInsideNet(Frame2D location)
+        public bool IsInsideNet(Frame3D location)
         {
             return location.Y < -100 && Math.Abs(location.X) < 57.2;
         }
 
-        double Distance(Frame2D first, Frame2D second)
+        double Distance(Frame3D first, Frame3D second)
         {
             return Math.Sqrt(Math.Pow(first.X - second.X, 2) + Math.Pow(first.Y - second.Y, 2));
         }
