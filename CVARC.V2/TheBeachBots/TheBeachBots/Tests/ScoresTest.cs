32854a9cc40a6c592877ed95f074914a99c8b782
Ivan Poroshin <niemandkun@yandex.ru>
2015-10-21 14:03:19 +0500

    scores test

---
+++ CVARC.V2/TheBeachBots/TheBeachBots/Tests/ScoresTest.cs
@@ -0,0 +1,18 @@
+﻿using CVARC.V2;
+
+namespace TheBeachBots
+{
+    partial class TBBLogicPartHelper
+    {
+        [TestLoaderMethod]
+        void LoadScoresTest(LogicPart logic, TBBRules rules)
+        {
+            var builder = new TBBTestBuilder(logic, rules, new TBBWorldState(0));
+
+            builder.CreateTest("Scores_Zero-IfNothingHappened")
+                .Stand(5)
+                .AssertScores(0)                
+                .EndOfTest();
+        }
+    }
+}
